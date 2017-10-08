2e143541d9a1cce2811013dc74ffb2f1c2926880
Colin Martin <colin.martin@gmail.com>
2017-10-08 11:40:21 -0500

    Revise required columns

--- filter_empty_investments.py
+++ filter_empty_investments.py
@@ -1,31 +1,39 @@
-import numpy as np
+#!/home/colin/work_tasks/bin/python
+
+# import numpy as np
 import pandas as pd
 from tkinter import filedialog
 openfile = filedialog.askopenfilename()
 df = pd.read_excel(openfile)
-required_columns = ['PAMPortCode',	'PortNameAlt', 'PolarID', 'SumOfUnits', 'secname', 'SumOfMarket Value', 'PamPrice',
-                    'FxShortName', 'AvgOfusfx', 'SumOfEnding Accrued', 'SumOfUnreal FX G/L Income']
+required_columns = ['Polar ID',
+                    'Units',
+                    'Security Description 1',
+                    'Cost',
+                    'Market Value',
+                    'M/E Price',
+                    'Accrued Int/Dvd',
+                    'Currency']
 
 #Step 1 - Create a new sheet with only the columns I want
 df1 = df[[*required_columns]]
+print(df1.head())
 
-
-df2 = df1.copy()
-
-#Strip out all unwanted funds
-df2 = df2[(df2['PortNameAlt'].str[-1].str.isdigit()) | (df2['PortNameAlt'] == "PMSLSF")]
-
-# Set the Market value to exclude all small amounts
-mv_eq = (df2['SumOfMarket Value'] < -0.25) | (df2['SumOfMarket Value'] > 0.25)
-
-# Set the accrual value to exclude all small amounts
-ac_eq = (df2['SumOfEnding Accrued'] < -0.25) | (df2['SumOfEnding Accrued'] > 0.25)
-
-# Filter rule. Exclude zero quantity. Exclude small MV's and Accrual's
-df2 = df2[(df2['SumOfUnits'] != 0) | (mv_eq) | (ac_eq)]
-
-savefile = filedialog.asksaveasfilename()
-writer = pd.ExcelWriter(savefile)
-df2.to_excel(writer, index=False, sheet_name='Pasting')
-df1.to_excel(writer, index=False, sheet_name='Original')
-writer.save()
+# df2 = df1.copy()
+#
+# #Strip out all unwanted funds
+# df2 = df2[(df2['PortNameAlt'].str[-1].str.isdigit()) | (df2['PortNameAlt'] == "PMSLSF")]
+#
+# # Set the Market value to exclude all small amounts
+# mv_eq = (df2['SumOfMarket Value'] < -0.25) | (df2['SumOfMarket Value'] > 0.25)
+#
+# # Set the accrual value to exclude all small amounts
+# ac_eq = (df2['SumOfEnding Accrued'] < -0.25) | (df2['SumOfEnding Accrued'] > 0.25)
+#
+# # Filter rule. Exclude zero quantity. Exclude small MV's and Accrual's
+# df2 = df2[(df2['SumOfUnits'] != 0) | (mv_eq) | (ac_eq)]
+#
+# savefile = filedialog.asksaveasfilename()
+# writer = pd.ExcelWriter(savefile)
+# df2.to_excel(writer, index=False, sheet_name='Pasting')
+# df1.to_excel(writer, index=False, sheet_name='Original')
+# writer.save()
